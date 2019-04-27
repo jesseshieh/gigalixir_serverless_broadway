0668454b5d9f187deac620bde1e022bd8e3f462b
Jesse <jesse@gigalixir.com>
2019-04-27 07:33:03 -0700

    mix new

---
+++ lib/gigalixir_serverless_broadway.ex
@@ -0,0 +1,18 @@
+defmodule GigalixirServerlessBroadway do
+  @moduledoc """
+  Documentation for GigalixirServerlessBroadway.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GigalixirServerlessBroadway.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gigalixir_serverless_broadway_test.exs
@@ -0,0 +1,8 @@
+defmodule GigalixirServerlessBroadwayTest do
+  use ExUnit.Case
+  doctest GigalixirServerlessBroadway
+
+  test "greets the world" do
+    assert GigalixirServerlessBroadway.hello() == :world
+  end
+end
